5fd0f0bf9f0bc305a717927de37c483637002029
Macrdev04 <manuelcomtrabajo@gmail.com>
2025-05-22 16:48:11 -0400

    modificaciones de semantica a variables para usuario test

--- one_use.php
+++ one_use.php
@@ -8,12 +8,12 @@ $passwrd = "1234";  // PIN temporal, luego puedes cambiarlo
 $role = "admin"; // Nivel de acceso
 
 // Hashear el PIN con bcrypt
-$pin_bcrypt = password_hash($pin_plano, PASSWORD_BCRYPT);
+$pass_bcrypt = password_hash($passwrd, PASSWORD_BCRYPT);
 
 // Inserción
-$sql = "INSERT INTO users (u_name, email, passwrd, rol) VALUES (?, ?, ?, ?)";
+$sql = "INSERT INTO users (u_name, email, passwrd, role) VALUES (?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ssss", $nombre, $email, $pin_bcrypt, $rol);
+$stmt->bind_param("ssss", $nombre, $email, $pass_bcrypt, $role);
 
 if ($stmt->execute()) {
     echo "✅ Usuario administrador creado correctamente.";
